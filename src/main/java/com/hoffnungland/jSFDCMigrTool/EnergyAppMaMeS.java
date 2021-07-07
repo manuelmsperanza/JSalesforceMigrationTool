df8602e2d28d29fce1b74fdb0ec074f23903c590
manuelmsperanza <manu.speranz@gmail.com>
2021-07-07 12:15:45 +0200

    fixed error package com.sun.tools.sjavac is not visible

--- src/main/java/com/hoffnungland/jSFDCMigrTool/EnergyAppMaMeS.java
+++ src/main/java/com/hoffnungland/jSFDCMigrTool/EnergyAppMaMeS.java
@@ -1,14 +1,10 @@
 package com.hoffnungland.jSFDCMigrTool;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -21,7 +17,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -30,7 +25,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.tools.ant.Project;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -38,9 +32,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.hoffnungland.xpath.XmlExtractor;
-import com.salesforce.ant.DeployTask;
-import com.salesforce.ant.RetrieveTask;
-import com.sun.tools.sjavac.Log;
 
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.SaxonApiUncheckedException;
