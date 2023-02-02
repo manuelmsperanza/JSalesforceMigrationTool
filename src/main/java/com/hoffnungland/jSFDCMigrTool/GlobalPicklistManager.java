d163445bcb90b0fd800142fcfc24bd202f8c1ea4
manuelmsperanza <manu.speranz@gmail.com>
2023-02-02 10:31:43 +0100

    first draft GlobalPicklistManager

---
+++ src/main/java/com/hoffnungland/jSFDCMigrTool/GlobalPicklistManager.java
@@ -0,0 +1,23 @@
+package com.hoffnungland.jSFDCMigrTool;
+
+public class GlobalPicklistManager {
+	
+	/*	https://help.salesforce.com/s/articleView?id=sf.fields_references.htm&type=5
+		https://developer.salesforce.com/forums/?id=9060G000000MVLdQAO
+		https://github.com/regularcoder/deepfield/blob/master/classes/DeepFieldController.cls*/
+	
+	public static void main(String[] args) {
+		
+		/*1. Create new temporary string field*/
+		/*2. Bring values from local picklist field to temporary string field*/
+		/*3. Disable local picklist dependencies (where it's used)*/
+		/*4. Delete local picklist field*/
+		/*5. Install global picklist field*/
+		/*6. Bring back values from temporary string field to global picklist field*/
+		/*7. Enable global picklist dependencies*/
+		/*8. delete temporary string field*/
+		
+
+	}
+
+}
