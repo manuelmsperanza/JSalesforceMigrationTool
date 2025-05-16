d6112d8ea2ffaa1290b23725ba1d681cd51eb2f6
manuelmsperanza <manu.speranz@gmail.com>
2025-05-16 16:40:29 +0200

    fixed warnings

--- src/main/java/com/hoffnungland/jSFDCMigrTool/OrgMetadataToExcel.java
+++ src/main/java/com/hoffnungland/jSFDCMigrTool/OrgMetadataToExcel.java
@@ -6,10 +6,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
